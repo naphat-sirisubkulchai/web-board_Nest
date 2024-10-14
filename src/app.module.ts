dd004f2716498adad669d76126a521861b44e7ca
naphat-sirisubkulchai <getget01010@gmail.com>
2024-10-14 18:40:04 +0700

    add comment

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,14 @@ import { Module } from '@nestjs/common';
 import { PostsLogModule } from './posts-log/posts-log.module'; 
 import { PostModule } from './post/post.module'; 
 import { UserModule } from './user/user.module'; 
+import { CommentModule } from './comment/comment.module';
 
 @Module({
   imports: [
     PostsLogModule,    
     PostModule,         
     UserModule,         
-   
+    CommentModule
   ],
   controllers: [],
   providers: [],
